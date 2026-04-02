a0faa57b1e4d326c6b9ba16b3fccf9f63544695d
Grégoire Pineau <lyrixx@lyrixx.info>
2026-04-02 16:45:11 +0200

    --

--- src/Controller/TimeController.php
+++ src/Controller/TimeController.php
@@ -5,12 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
-use Symfony\Component\Routing\Attribute\Route;
 
 final class TimeController extends AbstractController
 {
     #[Cache(public: true, maxage: 10)]
-    #[Route('/time', name: 'app_time')]
     public function index(): Response
     {
         return new Response(date('H:i:s'));
