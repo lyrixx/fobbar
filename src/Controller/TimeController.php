0288fff7164547596095fe4d473c5d4e08549b93
Grégoire Pineau <lyrixx@lyrixx.info>
2026-04-02 16:57:36 +0200

    --

--- src/Controller/TimeController.php
+++ src/Controller/TimeController.php
@@ -5,12 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 final class TimeController extends AbstractController
 {
-    #[Cache(public: true, maxage: 10)]
+    public function __construct(
+        private CacheInterface $cache,
+    ) {
+    }
+
+    #[Cache(public: true, maxage: 5)]
     public function index(): Response
     {
-        return new Response(date('H:i:s'));
+        $time = $this->cache->get('current_time_', $this->getTime(...));
+
+        return new Response($time);
+    }
+
+    private function getTime(ItemInterface $item): string
+    {
+        $item->expiresAfter(10);
+
+        return date('H:i:s');
     }
 }
